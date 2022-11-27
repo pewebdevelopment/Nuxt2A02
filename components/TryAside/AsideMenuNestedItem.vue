ee7fb46c0990b0bde9caba3a010ef9a525db7385
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-11-27 20:11:30 +0530

    Cleaned, minimized AsideMenuNestedItem.vue

--- components/TryAside/AsideMenuNestedItem.vue
+++ components/TryAside/AsideMenuNestedItem.vue
@@ -1,19 +1,19 @@
 <script>
-export default {
-  inheritAttrs: false,
-};
+// export default {
+//   inheritAttrs: false,
+// };
 </script>
 
 <script setup>
 import { ref, computed, watch } from "vue";
-import { RouterLink } from "vue-router";
-import { mdiMinus, mdiPlus, mdiChevronRight } from "@mdi/js";
-import { useStyleStore } from "@/store/style.js";
-import { getButtonColor } from "@/configs/colors.js";
-import BaseIcon from "@/components/BaseIcon.vue";
-import PremAsideMenuList from "@/components/TryAside/AsideMenuList.vue";
-import PremUpdateMark from "@/components/TryAside/UpdateMark.vue";
-import PremAsideMenuNestedItem from "./AsideMenuNestedItem.vue";
+// import { RouterLink } from "vue-router";
+// import { mdiMinus, mdiPlus, mdiChevronRight } from "@mdi/js";
+// import { useStyleStore } from "@/store/style.js";
+// import { getButtonColor } from "@/configs/colors.js";
+// import BaseIcon from "@/components/BaseIcon.vue";
+// import PremAsideMenuList from "@/components/TryAside/AsideMenuList.vue";
+// import PremUpdateMark from "@/components/TryAside/UpdateMark.vue";
+// import PremAsideMenuNestedItem from "./AsideMenuNestedItem.vue";
 
 const props = defineProps({
   item: {
@@ -30,54 +30,54 @@ const props = defineProps({
 
 const emit = defineEmits(["menu-click"]);
 
-const styleStore = useStyleStore();
+// const styleStore = useStyleStore();
 
-const asideMenuItemInactiveStyle = computed(() =>
-  hasColor.value ? "" : `${styleStore.asideMenuItemInactiveStyle} dark:text-gray-300`
-);
+// const asideMenuItemInactiveStyle = computed(() =>
+//   hasColor.value ? "" : `${styleStore.asideMenuItemInactiveStyle} dark:text-gray-300`
+// );
 
-const asideMenuItemActiveBgStyle = computed(() => {
-  return `${styleStore.asideMenuItemActiveBgStyle} dark:bg-slate-700/25`;
-});
+// const asideMenuItemActiveBgStyle = computed(() => {
+//   return `${styleStore.asideMenuItemActiveBgStyle} dark:bg-slate-700/25`;
+// });
 
-const asideMenuItemActiveStyle = computed(() => {
-  console.log("stylestore:", stylestore);
-  hasColor.value ? "" : styleStore.asideMenuItemActiveStyle;
-});
+// const asideMenuItemActiveStyle = computed(() => {
+//   console.log("stylestore:", stylestore);
+//   hasColor.value ? "" : styleStore.asideMenuItemActiveStyle;
+// });
 
-const isAsideCompact = computed(() => props.isCompact);
+// const isAsideCompact = computed(() => props.isCompact);
 
-watch(isAsideCompact, (value) => {
-  if (value) {
-    isDropdownActive.value = false;
-  }
-});
+// watch(isAsideCompact, (value) => {
+//   if (value) {
+//     isDropdownActive.value = false;
+//   }
+// });
 
 const isDropdownActive = ref(false);
 
-const isSecondaryMenuActive = computed(
-  () =>
-    props.activeSecondaryMenuKey &&
-    props.item?.key &&
-    props.item?.key === props.activeSecondaryMenuKey
-);
+// const isSecondaryMenuActive = computed(
+//   () =>
+//     props.activeSecondaryMenuKey &&
+//     props.item?.key &&
+//     props.item?.key === props.activeSecondaryMenuKey
+// );
 
-const hasColor = computed(() => props.item && props.item.color);
+// const hasColor = computed(() => props.item && props.item.color);
 
-const componentClass = computed(() => {
-  const base = [
-    props.isDropdownList ? "py-3 px-6 text-sm" : "py-3",
-    hasColor.value
-      ? getButtonColor(props.item.color, false, true)
-      : `${styleStore.asideMenuItemStyle} dark:hover:bg-gray-700/50`,
-  ];
+// const componentClass = computed(() => {
+//   const base = [
+//     props.isDropdownList ? "py-3 px-6 text-sm" : "py-3",
+//     hasColor.value
+//       ? getButtonColor(props.item.color, false, true)
+//       : `${styleStore.asideMenuItemStyle} dark:hover:bg-gray-700/50`,
+//   ];
 
-  if (!hasColor.value && (isDropdownActive.value || isSecondaryMenuActive.value)) {
-    base.push(asideMenuItemActiveBgStyle.value);
-  }
+//   if (!hasColor.value && (isDropdownActive.value || isSecondaryMenuActive.value)) {
+//     base.push(asideMenuItemActiveBgStyle.value);
+//   }
 
-  return base;
-});
+//   return base;
+// });
 
 const hasDropdown = computed(() => !!props.item.menu);
 
@@ -85,13 +85,13 @@ const hasSecondary = computed(() => !!props.item.menuSecondary);
 
 const hasSub = computed(() => hasDropdown.value || hasSecondary.value);
 
-const subIcon = computed(() => {
-  if (hasSecondary.value) {
-    return mdiChevronRight;
-  }
+// const subIcon = computed(() => {
+//   if (hasSecondary.value) {
+//     return mdiChevronRight;
+//   }
 
-  return isDropdownActive.value ? mdiMinus : mdiPlus;
-});
+//   return isDropdownActive.value ? mdiMinus : mdiPlus;
+// });
 
 const menuClick = (event) => {
   console.log("menuClick:", props.item);
@@ -103,11 +103,11 @@ const menuClick = (event) => {
   }
 };
 
-const updateMarkPosition = computed(() => {
-  const base = "top-0.5 right-4";
+// const updateMarkPosition = computed(() => {
+//   const base = "top-0.5 right-4";
 
-  return props.isCompact ? `${base} lg:right-6` : base;
-});
+//   return props.isCompact ? `${base} lg:right-6` : base;
+// });
 </script>
 
 <template>
