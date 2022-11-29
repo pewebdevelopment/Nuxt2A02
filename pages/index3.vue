1133a9f05fdeb0d0830183aa4539108578f943ce
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-11-29 23:09:39 +0530

    h fun cant render a component in Vue 2, Nuxt 2

---
+++ pages/index3.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  <Index3Component>
+  </div>
+</template>
+
+<script>
+
+import Index3Component from '~/components/index3Component.vue';
+
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
